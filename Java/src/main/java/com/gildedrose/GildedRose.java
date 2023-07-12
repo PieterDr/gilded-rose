7cc3f47dc4c315969f717f0fa854e8b4f7e44f3c
PieterDr <pieter.drijkoningen@vrt.be>
2023-07-12 18:28:27 +0200

    Move all quality adjusting together

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -12,6 +12,9 @@ class GildedRose {
             switch (item.name) {
                 case "Aged Brie":
                     item.quality = Math.min(50, item.quality + 1);
+                    if (item.sellIn < 1) {
+                        item.quality = Math.min(50, item.quality + 1);
+                    }
                     break;
                 case "Backstage passes to a TAFKAL80ETC concert":
                     item.quality = Math.min(50, item.quality + 1);
@@ -21,33 +24,23 @@ class GildedRose {
                     if (item.sellIn < 6) {
                         item.quality = Math.min(50, item.quality + 1);
                     }
+                    if (item.sellIn < 1) {
+                        item.quality = 0;
+                    }
                     break;
                 case "Sulfuras, Hand of Ragnaros":
                     break;
                 default:
                     item.quality = Math.max(0, item.quality - 1);
+                    if (item.sellIn < 1) {
+                        item.quality = Math.max(0, item.quality - 1);
+                    }
                     break;
             }
 
             if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
             } else {
                 item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                switch (item.name) {
-                    case "Aged Brie":
-                        item.quality = Math.min(50, item.quality + 1);
-                        break;
-                    case "Backstage passes to a TAFKAL80ETC concert":
-                        item.quality = 0;
-                        break;
-                    case "Sulfuras, Hand of Ragnaros":
-                        break;
-                    default:
-                        item.quality = Math.max(0, item.quality - 1);
-                        break;
-                }
             }
         }
     }
